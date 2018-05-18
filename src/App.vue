5b2a0706c9dcf1aa93a24a904e75454c70fb93fe
Igor Oliinyk <igor.oleynikdp@gmail.com>
2018-05-18 13:30:33 +0300

    Разбил приложения на компоненты. Компоненты разложены по папкам по логике Добавлен компонент Footer

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,27 @@
 <template>
     <div class="container">
-        <div class="row">
-            <div class="col-xs-12">
-              <header>
-                <h1>Server Status</h1>
-              </header>
-            </div>
-        </div>
+        <Header></Header>
         <hr>
         <div class="row">
-          <div class="col-xs-12 col-sm-6">
-            <ul class="list-group">
-              <li class="list-group-item"
-                v-for="index in 5">
-                Server #{{ index }}
-              </li>
-            </ul>
-          </div>
-          <div class="col-xs-12 col-sm-6">
-            <p>Server Details Currently not updated</p>
-          </div>
+          <ServerList></ServerList>
+          <ServerStatus></ServerStatus>
         </div>
+        <Footer></Footer>
     </div>
 </template>
 
 <script>
+  import Header from './components/Shared/Header.vue'
+  import Footer from './components/Shared/Footer.vue'
+  import ServerStatus from './components/Server/ServerStatus.vue'
+  import ServerList from './components/Server/ServerList.vue'
     export default {
+      components: {
+        Header,
+        Footer,
+        ServerStatus,
+        ServerList
+      }
     }
 </script>
 
---
+++ src/components/Shared/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="col-xs-12">
+    <p>All servers are managed here</p>
+  </div>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style scoped>
+
+</style>
