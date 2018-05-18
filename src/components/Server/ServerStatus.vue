3d7ce84b84f967523163aee6c23efa95d2bd7c1e
Igor Oliinyk <igor.oleynikdp@gmail.com>
2018-05-19 00:32:56 +0300

    Добавляем кнопку для установки статуса Normal Редактируется через объект переданный по ссылке Тут нам не нужно передавать сообщения между объектами

--- src/components/Server/ServerStatus.vue
+++ src/components/Server/ServerStatus.vue
@@ -2,6 +2,8 @@
   <div class="col-xs-12 col-sm-6">
     <p v-if="!server">Please select server</p>
     <p v-else>Server {{ server.id }} selected. Status: {{ server.status }}</p>
+    <hr>
+    <button v-if="server" @click="resetStatus">Change status to Normal</button>
   </div>
 </template>
 
@@ -11,6 +13,11 @@
     data() {
       return {
         server: null
+      }
+    },
+    methods: {
+      resetStatus() {
+        this.server.status = 'Normal';
       }
     },
     created() {
